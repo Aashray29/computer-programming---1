85a6087f1c0684c47c4c4a2e0c749d6164231f7a
Aashray29 <aashrayranpara29@gmail.com>
2025-08-20 20:17:26 +0530

    Fahrenheittocelcuis.cpp

---
+++ Fahrenheittocelcuis.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+float main(){
+  float a,b;
+clrscr();
+printf("enter fehrenheit");
+scanf("%f",&a);
+b = 0.5555*a - 17.7777;
+printf("celcuis = %f",b);
+getch();
+}
