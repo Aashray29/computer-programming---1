fad9a990e5464d60ee86cd2162f1cf282caa7524
Aashray29 <aashrayranpara29@gmail.com>
2025-08-20 22:17:14 +0530

    Square.cpp
    This programm is about to find square 's area and perimeter.

---
+++ Square.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+float main(){
+  float a,b,c;
+  clrscr();
+printf("enter square's side length: ");
+scanf("%f",&a);
+b = a*a;
+printf("area of square is: %f\n",b);
+c = 4*a;
+printf("perimeter of square is: %f",c);
+  getch();
+}
