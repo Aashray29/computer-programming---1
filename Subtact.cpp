ec2608904ad6f2954a98ec057592ce7eb176ad9e
Aashray29 <aashrayranpara29@gmail.com>
2025-08-20 13:31:28 +0530

    Subtact.cpp

---
+++ Subtact.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main() {
+int a,b,c;
+  clrscr();
+printf("enter 2 nos.");
+scanf("%d %d",&a,&b);
+c = a - b;
+printf("%d - %d = %d\n",a,b,c);
+  getch();
+}
