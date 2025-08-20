f18745a9fc70a37e1217de54c45d4432913c62be
Aashray29 <aashrayranpara29@gmail.com>
2025-08-20 20:14:36 +0530

    Celciustofahrenheit.cpp

---
+++ Celciustofahrenheit.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+float main(){
+  float a,b;
+clrscr();
+printf("enter celcius");
+scanf("%f",&a);
+b = a*1.8 + 57.6;
+printf("fehrenheit = %f",b);
+getch();
+}
