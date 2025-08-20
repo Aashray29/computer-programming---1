5bd1f7cd1747cd53e54f853362986272ef89c60f
Aashray29 <aashrayranpara29@gmail.com>
2025-08-20 19:49:44 +0530

    Division.cpp

---
+++ Division.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+float main() {
+float a,b,c;
+  clrscr();
+printf("enter 2 nos.");
+scanf("%f %f",&a,&b);
+c = a / b;
+printf("%f / %f = %f\n",a,b,c);
+  getch();
+}
