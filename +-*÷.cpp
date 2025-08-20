fc2dc37e0014746927fdea110782c55412e25c13
Aashray29 <aashrayranpara29@gmail.com>
2025-08-20 19:57:20 +0530

    +-*÷.cpp

---
+++ +-*÷.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+float main() {
+float a,b,c,d,e,f;
+  clrscr();
+printf("enter 2 nos.");
+scanf("%f %f",&a,&b);
+c = a - b;
+printf("%f - %f = %f\n",a,b,c);
+  printf("enter 2 nos.");
+scanf("%f %f",&a,&b);
+d = a + b;
+printf("%f - %f = %f\n",a,b,d);
+  printf("enter 2 nos.");
+scanf("%f %f",&a,&b);
+e = a * b;
+printf("%f - %f = %f\n",a,b,e);
+  printf("enter 2 nos.");
+scanf("%f %f",&a,&b);
+f = a / b;
+printf("%f - %f = %f\n",a,b,f);
+  getch();
+}
