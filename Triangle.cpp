dc14daa332112c7eca34a7abd4b4cd00d8eca106
Aashray29 <aashrayranpara29@gmail.com>
2025-08-21 19:46:15 +0530

    Triangle.cpp

---
+++ Triangle.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+float main(){
+  float r,a,p;
+  clrscr();
+printf("enter base: ");
+scanf("%f",&r);
+printf("enter height: ");
+scanf("%f",&a);
+p = (a*r)/2;
+printf("area of triangle is: %f\n",p);
+  getch();
+}
