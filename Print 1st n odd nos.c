2f7da60753398961b3b1de42f911331d9cfe1628
Aashray Ranpara <aashrayranpara29@gmail.com>
2025-08-25 23:35:23 +0530

    Print 1st n odd nos.c

---
+++ Print 1st n odd nos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int count = 1;   
+    int num = 1;   
+    int n;
+    printf("enter number ");  
+    scanf("%d",&n);
+
+    while (count <= n) {
+        printf("%d ", num);
+        num += 2;    
+        count++;
+    }
+
+    return 0;
+}
