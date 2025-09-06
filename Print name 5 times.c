9ded1e40cb971ecb8601469a511b9074b2fc05d1
Aashray Ranpara <aashrayranpara29@gmail.com>
2025-09-06 18:13:13 +0530

    Print name 5 times.c

---
+++ Print name 5 times.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+char main() {
+    char n[10];
+    printf("Enter your name: ");
+    scanf("%s", &n);
+    int i;
+    for(i = 1; i<=5; i++) {
+    
+    printf("your name  is  = %s\n", n);
+    }
+    getch();
+}
